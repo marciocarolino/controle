02c0b98a5bc93eb828bf1fc385ab855f11fc1697
marcio carolino <marciocarolino@gmail.com>
2022-09-25 16:33:13 -0300

    primeira rota backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RevenuModule } from './app/revenue/revenuce.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [RevenuModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ backend/src/app/revenue/revenue.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { RevenueService } from './revenue.service';
+
+@Controller('revenue')
+export class RevenueController {
+  constructor(private readonly revenueService: RevenueService) {}
+}
